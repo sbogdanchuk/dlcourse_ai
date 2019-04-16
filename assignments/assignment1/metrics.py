7ba2f69315231a64b1af00b2726dc80bfa82d474
sbogdanchuk <sergey.bogdanchuk@your.id>
2019-04-16 19:53:48 +0300

    feat: metrics

--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -9,16 +9,32 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
 
     # TODO: implement metrics!
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
     
+    TP=0
+    TN=0
+    FP=0
+    FN=0
+    total=len(prediction)
+    
+    for predicted, actual in zip(prediction, ground_truth):
+        if predicted and actual:
+            TP+=1
+        if not(predicted and actual):
+            TN+=1
+        if predicted and not actual:
+            FP+=1
+        else:
+            FN+=1
+            
+    precision = TP/sum(prediction)
+    recall = TP/sum(ground_truth)
+    accuracy = (TP+TN)/total
+    f1 = 2*recall*precision/(precision+recall)
     return precision, recall, f1, accuracy
 
 
